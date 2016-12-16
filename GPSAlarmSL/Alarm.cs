c730d64e1995169cf6371e3d017336168a5f145d
A-Kinski <asidorov90@gmail.com>
2016-12-16 21:44:42 +0300

    First MVP before test on Device - fix Alarm class

--- GPSAlarmSL/Alarm.cs
+++ GPSAlarmSL/Alarm.cs
@@ -10,23 +10,58 @@ namespace GPSAlarmSL
 {
     class GpsAlarm
     {
+        private Alarm alarm;
+
         public GpsAlarm()
         {
+            alarm = new Alarm("destinationAlarm");
+            alarm.Content = "Приехали";
+            alarm.Sound = new Uri("/Assets/Media/Ring01.wma", UriKind.Relative);
+            //alarm.BeginTime = DateTime.Now.AddSeconds(5);
+            //alarm.ExpirationTime = alarm.BeginTime.AddMinutes(2);
+            alarm.RecurrenceType = RecurrenceInterval.None;
+
+            //ScheduledActionService.Add(alarm);
         }
 
         public void createAlarm()
         {
-            Alarm alarm = new Alarm("destinationAlarm");
-            alarm.Content = "Приехали";
-            alarm.Sound = new Uri("/Assets/Media/Ring01.wma", UriKind.Relative);
-            alarm.BeginTime = DateTime.Now.AddSeconds(5);
+            //Alarm alarm = new Alarm("destinationAlarm");
+            //alarm.Content = "Приехали";
+            //alarm.Sound = new Uri("/Assets/Media/Ring01.wma", UriKind.Relative);
+            //alarm.BeginTime = DateTime.Now.AddSeconds(5);
+            //alarm.ExpirationTime = alarm.BeginTime.AddMinutes(2);
+            //alarm.RecurrenceType = RecurrenceInterval.None;
+
+            //if (ScheduledActionService.Find("destinationAlarm") != null)
+            //{
+            //    var oldAlarm = ScheduledActionService.Find("destinationAlarm");
+            //    if (oldAlarm.BeginTime < DateTime.Now)
+            //    {
+            //        ScheduledActionService.Remove("destinationAlarm");
+            //        oldAlarm.BeginTime = DateTime.Now.AddSeconds(5);                    
+            //        ScheduledActionService.Add(oldAlarm);
+            //    }
+
+            //}
+            //else
+            //{
+            // //   ScheduledActionService.Add(alarm);
+            //}
+
+            alarm.BeginTime = DateTime.Now.AddSeconds(20);
             alarm.ExpirationTime = alarm.BeginTime.AddMinutes(2);
-            alarm.RecurrenceType = RecurrenceInterval.None;
 
             if (ScheduledActionService.Find("destinationAlarm") != null)
             {
-                var oldAlarm = ScheduledActionService.Find("destinationAlarm");
-                if (oldAlarm.BeginTime < DateTime.Now) oldAlarm.BeginTime = DateTime.Now.AddSeconds(5);
+                if (ScheduledActionService.Find("destinationAlarm").BeginTime < DateTime.Now)
+                {
+                    Alarm bufferAlarm = ScheduledActionService.Find("destinationAlarm") as Alarm;
+                    bufferAlarm.BeginTime = DateTime.Now.AddSeconds(20);
+                    bufferAlarm.ExpirationTime = alarm.BeginTime.AddMinutes(2);
+                    bufferAlarm.Content = "123";
+                    ScheduledActionService.Replace(bufferAlarm);
+                }
             }
             else
             {
